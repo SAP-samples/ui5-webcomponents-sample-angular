c654f60794e128a245f1a2a6722249120c98a39a
Adrian Bobev <adrian.bobev@gmail.com>
2019-04-23 16:56:28 +0300

    chore: update ui5-webcomponents version to 0.10.0 (#10)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { ListComponent } from './list/list.component';
 
-import "@ui5/webcomponents-base/src/sap/ui/webcomponents/base/browsersupport/Edge";
+import "@ui5/webcomponents-base/src/browsersupport/Edge";
 // UI5 Web Components used
 import '@ui5/webcomponents/dist/Button';
 import '@ui5/webcomponents/dist/ShellBar';
