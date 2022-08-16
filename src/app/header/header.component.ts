a3ffaed6be503b2e997ecfa9ba7fa0b35802df57
hinzzx <abvsuxhard@gmail.com>
2022-08-16 12:28:33 +0300

    feat: profile menu on click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,6 +11,7 @@ import {setTheme } from '@ui5/webcomponents-base/dist/config/Theme';
 export class HeaderComponent implements OnInit {
 	@Input() selectionChange;
 	@ViewChild('themeSettingsPopover') themeSettingsPopover: ElementRef;
+	@ViewChild('profileSettingsPopover') profileSettingsPopover: ElementRef;
 
 	constructor() { }
 
@@ -26,4 +27,36 @@ export class HeaderComponent implements OnInit {
 		this.themeSettingsPopover.nativeElement.close();
 	}
 
+	handleProfileClick(event) {
+		this.profileSettingsPopover.nativeElement.showAt(event.detail.targetRef);
+	}
+
+	handleProfileSettingsSelect(event) {
+		const selectedKey = event.detail.item.getAttribute('data-key');
+		if (selectedKey === "settings") {
+			window["settings-dialog"].show(event.detail.targetRef);
+		 } else if (selectedKey === "help") {
+			window["help-dialog"].show(event.detail.targetRef);
+		}
+	}
+
+	handleRtlSwitchChange(event) {
+		document.body.dir = event.target.checked ? "rtl" : "ltr";
+	}
+
+	handleContentDensitySwitchChange(event) {
+		if (event.target.checked) {
+			document.body.classList.add("ui5-content-density-compact");
+		} else {
+			document.body.classList.remove("ui5-content-density-compact");
+		}
+	}
+
+	handleSettingsDialogCloseButtonClick(event){
+		window["settings-dialog"].close();
+	}
+
+	handleHelpDialogCloseButtonClick(event){
+		window["help-dialog"].close();
+	}
 }
